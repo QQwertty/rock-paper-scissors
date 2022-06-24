25e712310330ed89d3c930f1e0ea531ca034ba06
Enoh <enohmihulet@gmail.com>
2022-06-24 15:34:22 -0500

    It is done

--- script.js
+++ script.js
@@ -30,21 +30,25 @@ document.addEventListener('DOMContentLoaded', () => {
   let l = 0
   const score = document.createElement('p')
   const final = document.createElement('p')
-
+  
+  
+  
   buttons.forEach((button)=> {
     button.addEventListener('click', (e) => {
+      if (w<5 && l<5) {
       const playerSelection = e.target.className
       rockPaperScissors(computerPlay(), playerSelection)
-  
+  }
     });
   });
-
-  function hide() {
+  
     
-    buttons.forEach((button)=> {button.remove()})
-    para.remove()
-    score.remove()
-  }
+  // function hide() {
+    
+  //   buttons.forEach((button)=> {button.remove()})
+  //   para.remove()
+  //   score.remove()
+  // }
   
   
 
@@ -57,6 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
         //return round = 'lose'
         para.textContent = `You lost, ${computerSelection} beats ${playerSelection}`
         score.textContent = `${w}-${++l}`
+        
     } 
     else if (computerSelection === 'rock' && playerSelection  === 'paper' || computerSelection === 'scissors' && playerSelection  === 'rock' || computerSelection === 'paper' && playerSelection  === 'scissors') {
         
@@ -65,40 +70,74 @@ document.addEventListener('DOMContentLoaded', () => {
         //return round = 'win'
         para.textContent = `You won, ${playerSelection} beats  ${computerSelection}`
         score.textContent = `${++w}-${l}`
+        
     }
     else if (computerSelection === 'rock' && playerSelection  === 'rock' || computerSelection === 'scissors' && playerSelection  === 'scissors' || computerSelection === 'paper' && playerSelection  === 'paper') {
         
         console.log('tie')
         //return  round = 'tie'
         para.textContent = "It's a tie"
+        score.textContent = `${w}-${l}`
         t++
+        
     } else {
-        return console.log('error')
+        console.log('error')
     }
+
     if (w == 5) {
   
       
       final.textContent = 'You beat the Computer!'
-      hide()
-      const restart = document.createElement('button')
-      restart.textContent = 'Restart'
-      body.appendChild(restart)
+      //hide()
+      
+      
+      return
     } else if (l == 5) {
     
       
       final.textContent = 'You lost to the Computer!'
-      hide()
-      const restart = document.createElement('button')
-      restart.textContent = 'Restart'
-      body.appendChild(restart)
+      //hide()
+      
+      
+      return
     } 
 };
-
-
-
-
 body.appendChild(score)
 body.appendChild(final)
+
+
+
+// restart.addEventListener('click', () => {
+//   const buttons = document.createElement('div');
+//   buttons.id = 'container'
+//   const rock = document.createElement('button')
+//   rock.className = 'rock'
+//   rock.textContent = 'Rock'
+//   const paper = document.createElement('button')
+//   paper.className = 'paper'
+//   paper.textContent = 'Paper'
+//   const scissors = document.createElement('button')
+//   scissors.className = 'scissors'
+//   scissors.textContent = 'Scissors'
+//   const para = document.createElement('p')
+//   para.className = 'winloss'
+//   let t = 0
+//   let w = 0
+//   let l = 0
+//   const score = document.createElement('p')
+//   final.remove()
+//   restart.remove()
+  
+//   body.appendChild(buttons)
+//   buttons.appendChild(rock)
+//   buttons.appendChild(paper)
+//   buttons.appendChild(scissors)
+//   body.appendChild(para)
+//   body.appendChild(score)
+
+// })
+})
+
 
 
 // function game() {
@@ -128,4 +167,4 @@ body.appendChild(final)
 //     console.log("It's a tie")
 //   }
 // }
- });
+ 
